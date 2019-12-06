4bd938a0629c1b2b941513b4cd5203cb789d860a
pouget <nicolaspouget@orange.fr>
2019-12-06 14:47:48 +0100

    Modification classe Frame Création classe FrameTest

--- src/main/java/miage/npou/Frame.java
+++ src/main/java/miage/npou/Frame.java
@@ -2,21 +2,72 @@ package miage.npou;
 
 public class Frame {
 
-    private int lance1;
+    private Integer lance1;
 
-    private int lance2;
+    private Integer lance2;
 
     private boolean strike;
 
     private boolean spare;
 
-    public Frame (int l1, int l2){
+    public Frame (Integer l1, Integer l2){
+
+        this.lance1 = l1;
+
+        this.strike = (l1==10);
+
+        //En cas de strike on instacie le lance 2 à null
+        if (isStrike()){
+            this.lance2 = null;
+        }else{
+            int somme = l1+l2;
+            //Si la somme est supérieure à 10 on donne le reste de 10 moins le lancé 1 à 2
+            if (somme>11){
+                this.lance2 = 10 - l1;
+            }else {
+                this.lance2 = l2;
+            }
+        }
+
+
+        if (l2!=null) {
+            this.spare = (this.lance1 + this.lance2 == 10);
+        }
 
     }
 
+
     public int getScore(){
 
-        return 0;
+        int score = 0;
+
+        if (isStrike()){
+
+            score += 10;
+
+        }else{
+
+            score += this.lance1 + this.lance2;
+
+        }
+
+        return score;
+    }
+
+    public Integer getLance1(){
+        return this.lance1;
+    }
+
+    public Integer getLance2(){
+        return this.lance2;
+    }
+
+    public boolean isSpare() {
+        return spare;
+    }
+
+    public boolean isStrike() {
+        return strike;
     }
 
 }
---
+++ src/test/java/miage/npou/FrameTest.java
@@ -0,0 +1,89 @@
+package miage.npou;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FrameTest {
+
+    private Frame aFrame;
+
+    @Before
+    public void setUp() throws Exception {
+        //Given : Création d'une frame
+        aFrame = new Frame(7,3);
+    }
+
+    @Test
+    public void getScoreTest() {
+        // When : on demande le score
+        int score = aFrame.getScore();
+
+        // Then : on vérifie que le score est bien de 10
+        assertEquals(10, score);
+
+        // And : on vérifie que ce n'est pas null
+        assertNotNull(score);
+
+        // When : En cas de strike sur la partie
+        aFrame = new Frame(new Integer(10), null);
+
+        // Then : on vérifie que le score est bien de 10
+        assertEquals(10, score);
+
+        // When : En cas de strike sur la partie
+        aFrame = new Frame(new Integer(9), new Integer(2));
+
+        // Then : on vérifie que le score est bien de 10
+        assertEquals(10, score);
+    }
+
+    @Test
+    public void getLance1Test(){
+        // When : on demande le lancé 1
+        int l1 = aFrame.getLance1();
+
+        // Then : on vérifie qu'il est bien égal à 7
+        assertEquals(7, l1);
+
+        // And : on vérifie qu'il n'est pas égal à 5
+        assertNotNull(l1);
+    }
+
+    @Test
+    public void getLance2Test(){
+        // When : on demande le lancé 2
+        Integer l2 = aFrame.getLance2();
+
+        // Then : on vérifie qu'il est bien égal à 7
+        assertEquals(new Integer(3), l2);
+
+        // And : on vérifie qu'il n'est pas null
+        assertNotNull(l2);
+    }
+
+    @Test
+    public void isSpareTest(){
+        // When : une frame est un spare
+        boolean isSpare = aFrame.isSpare();
+
+        // Then : Vérifier que c'est bien un spare
+        assertEquals(true, isSpare);
+
+        // And : que isSpare n'est pas null
+        assertNotNull(isSpare);
+    }
+
+    @Test
+    public void isStrikeTest(){
+        // When : une frame est un strike
+        boolean isStrike = aFrame.isStrike();
+
+        // Then : Vérifier que c'est bien un spare
+        assertEquals(false, isStrike);
+
+        // And : que isSpare n'est pas null
+        assertNotNull(isStrike);
+    }
+}
